0bd63259a57cfee2caf3948c1acdbc118f0f3ca0
unknown <sunil>
2025-07-14 01:33:28 +0530

    third commit

--- src/main/java/com/jenkins/demo/DemoApplication.java
+++ src/main/java/com/jenkins/demo/DemoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("springboot started");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
