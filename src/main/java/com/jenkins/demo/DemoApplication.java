d2077e2beb3b2777aeb4de96458f9d797c6438a6
unknown <sunil>
2025-07-14 01:39:31 +0530

    add log

--- src/main/java/com/jenkins/demo/DemoApplication.java
+++ src/main/java/com/jenkins/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 package com.jenkins.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	public static Logger  logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
+		logger.info("application satrted");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
